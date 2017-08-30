5e057aa58ba9df7bbb60e9c1acd3941de69bc35a
C-Aniruddh <aniruddh.chandratre@gmail.com>
2017-08-30 05:42:56 +0000

    garlic: build GNSS HAL

--- device.mk
+++ device.mk
@@ -202,6 +202,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
 
+# GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
