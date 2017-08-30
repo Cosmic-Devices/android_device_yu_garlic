1cdd9ab0a89bbdb8d25f77a85d522fcce82b9191
C-Aniruddh <aniruddh.chandratre@gmail.com>
2017-08-30 05:40:15 +0000

    garlic: build gatekeeper HAL

--- device.mk
+++ device.mk
@@ -196,7 +196,11 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-impl 
+
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
