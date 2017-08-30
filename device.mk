282bda4dca4bf2d48a4079a98a66e9cbb9d59c55
C-Aniruddh <aniruddh.chandratre@gmail.com>
2017-08-30 05:37:16 +0000

    garlic: build DRM HAL

--- device.mk
+++ device.mk
@@ -192,6 +192,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+
 # Misc
 PRODUCT_PACKAGES += \
     libcurl
