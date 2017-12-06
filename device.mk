b45c1aa914c5dc15a314e09be40e6c66795893d7
Sagar Shah <sagarshah2326@gmail.com>
2017-12-06 22:21:26 +0530

    Garlic: Add Fingerprint Wrapper[HACK]
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- device.mk
+++ device.mk
@@ -157,7 +157,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    fingerprint.msm8937
 
 # FM
 PRODUCT_PACKAGES += \
